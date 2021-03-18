320879d5e0c4835dd417a371fd9160328494c3cf
Victoria Ogunbiyi <46796521+Oviyon@users.noreply.github.com>
2021-03-18 12:26:34 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 require('dotenv').config();
 
 const express = require('express');
@@ -8,7 +8,7 @@ const SERVER_PORT = 4000;
 app.use(express.static('public'));
 
 app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
-=======
+
 require('dotenv').config();
 
 const express = require('express');
@@ -18,4 +18,4 @@ const SERVER_PORT = 4000;
 app.use(express.static('public'));
 
 app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
->>>>>>> 43d899db1e39cdccbd2827fc559d68b20e3eb3de
+
