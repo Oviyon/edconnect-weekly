84966fa885da6c45580040a32c3e85b11ceae850
mac <viyon.ogunbiyi@gmail.com>
2021-03-20 13:43:23 +0100

    Styled HTML pages for Project Explorer in Bootstrap

--- app/index.js
+++ app/index.js
@@ -9,13 +9,3 @@ app.use(express.static('public'));
 
 app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
 
-require('dotenv').config();
-
-const express = require('express');
-const app = express();
-const SERVER_PORT = 4000;
-
-app.use(express.static('public'));
-
-app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
-
